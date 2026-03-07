91272fcd3f570b9f647b0d0f7949a1272f8a0ea4
GlobalAiShine <globalyi.shine@gmail.com>
2026-03-08 02:29:33 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ProductDashboard from './product-ai';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ProductDashboard />
+  </React.StrictMode>
+);
